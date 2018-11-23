612f9c45a2c44c92fd856827c303aa062cf115ec
Dmitriy Efimenko <dmitriy.efimrnko@nixsolution.com>
2018-11-23 13:44:41 +0200

    objects-task

--- src/tasks/3_objects.js
+++ src/tasks/3_objects.js
@@ -2,31 +2,60 @@
  * Функция принимает объект, имя свойства которое нужно изменить и новое имя свойства
  * Функция должна "переименовать" свойство в полученном объекте
  */
-function renameProperty(obj, propName, newName) {}
+function renameProperty(obj, propName, newName) {
+  obj[newName] = obj[propName];
+  delete obj[propName];
+  return obj[newName];
+
+}
 
 /**
  * Функция принимает объект
  * Функция должна вернуть количество свойств объекта (числом)
  */
-function countKeys(obj) {}
+function countKeys(obj) {
+  let countOfProps = 0;
+  for (let key in obj) {
+    countOfProps++;
+  }
+}
 
 /**
  * Функция принимает объект
  * Функция должна вернуть true если в объекте нет свойств или false в противном случае
  */
-function isEmpty(obj) {}
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
 
 /**
  * Функция принимает объект
  * Функция должна вернуть сумму значений свойств, где значение - число
  */
-function numericPropsSum(obj) {}
+function numericPropsSum(obj) {
+  let sum = 0;
+  for (let key in obj) {
+    if (typeof obj[key] == "number") {
+      sum += obj[key];
+    }
+  }
+  return sum;
+}
 
 /**
  * Функция принимает объект
  * Функция должна вернуть независимую копию объекта
  */
-function clone(obj) {}
+function clone(obj) {
+  let cloneObj = {};
+  for (let key in obj) {
+    cloneObj[key] = obj[key];
+  }
+  return cloneObj;
+}
 
 module.exports = {
   renameProperty,
