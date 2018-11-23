cb604c56f25503021455427235437f3b31225911
Victoria Sokolova <viktoriia.sokolova@nure.ua>
2018-11-23 12:31:16 +0200

    String tasks done

--- src/tasks/2_strings.js
+++ src/tasks/2_strings.js
@@ -2,26 +2,38 @@
  * Функция принимает id, имя и диаметр планеты
  * Функция должна вернуть строку в формате "<id><tab>|<name>|<tab><diameter>"
  */
-function constructPlanetInfoString(id, name, diameter) {}
+function constructPlanetInfoString(id, name, diameter) {
+  return `${id}\t|${name}|\t${diameter}`;
+}
 
 /**
  * Функция принимает какой-то текст
  * Функция должна вернуть true, если текст содержит слова из списка spamWords или false если не содержит.
  * Функция не должна быть чувствительна к регистру (если слово в списке спама "новинка", а в тексте "НоВиНкА" - то это спам)
  */
-function isSpam(text, spamWords) {}
+function isSpam(text, spamWords) {
+  for (let i = 0; i < spamWords.length; i++) {
+    if (text.toLowerCase().indexOf(spamWords[i].toLowerCase()) === -1)
+      return false;
+  }
+  return true;
+}
 
 /**
  * Функция принимает текст, и значение которое нужно подставить между {{ и }}
  * Функция должна вернуть текст с подставленным значением (interpolateString("Hello {{username}}", "Jason") => "Hello Jason")
  */
-function interpolateString(text, value) {}
+function interpolateString(text, value) {
+  return `${text.substr(0, text.indexOf("{{"))}${value}${text.substring(text.indexOf("}}") + 2)}`.trim();
+}
 
 /**
  * Функция принимает массив чисел, которые представляют собой коды символов
  * Функция должна вернуть текст, составленный из кодов символов. [1076, 1091, 1073] => "дуб"
  */
-function fromCharCodes(charCodes) {}
+function fromCharCodes(charCodes) {
+  return String.fromCharCode(...charCodes);
+}
 
 module.exports = {
   constructPlanetInfoString,
