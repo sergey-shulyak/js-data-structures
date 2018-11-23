80577f9b76ccc5c53dd18feed4b02be9105f95b5
Vlad Malyarenko <vladislav.malyarenko@nixsolutions.com>
2018-11-23 17:06:22 +0200

    Test 4 Add to second task test test on {{{{{

--- src/tasks/5_dates.js
+++ src/tasks/5_dates.js
@@ -5,14 +5,38 @@ require("../polyfills/intl");
  * Функция должна вернуть день недели (например "вторник"), который был ровно 10 лет назад
  * Используйте Intl: https://learn.javascript.ru/intl
  */
-function timeTravel(dateString) {}
+function timeTravel(dateString) {
+  let days = [
+    "воскресенье",
+    "понедельник",
+    "вторник",
+    "среда",
+    "четверг",
+    "пятница",
+    "суббота"
+  ];
+  let date = new Date(dateString);
+
+  date.setFullYear(date.getFullYear() - 10);
+  return days[date.getDay()];
+}
 
 /**
  * Функция принимает дату - число миллисекунд, прошедших с 1 января 1970 года
  * Функция должна вернуть строку в формате "ср, 21 нояб. 2018 г., 18:48"
  * Используйте Intl: https://learn.javascript.ru/intl
  */
-function formatDate(millis) {}
+function formatDate(millis) {
+  const formatter = new Intl.DateTimeFormat("ru", {
+    weekday: "narrow",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric"
+  });
+  return formatter.format(new Date(millis)).toString();
+}
 
 /**
  * Функция принимает функцию для которой нужно провести бенчмарк
