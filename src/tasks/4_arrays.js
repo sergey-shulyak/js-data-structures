2aaab781d54b3af7fecff3502618bf26a7ce160f
Vlad Malyarenko <vladislav.malyarenko@nixsolutions.com>
2018-11-23 16:17:12 +0200

    Task 4. Complete.

--- src/tasks/4_arrays.js
+++ src/tasks/4_arrays.js
@@ -10,32 +10,66 @@ function trueLength(array) {
  * Функция принимает строку, содержащую последовательность круглых, квадратных или фигурных скобок - "let obj = { numArray: [ ((1 + 2) * 5) + (42).toString() ] }"
  * Функция должна вернуть true если все скобки закрыты в правильном порядке или false в противном случае
  */
-function isExpressionValid(expression) {}
+function isExpressionValid(expression) {
+  let stack = [];
+  let currIndex;
+  let openTags = ["{", "(", "["];
+  let closeTags = ["}", ")", "]"];
+
+  for (let letter of expression) {
+    if (openTags.includes(letter)) {
+      stack.push(letter);
+    } else if (closeTags.includes(letter)) {
+      currIndex = openTags.indexOf(stack[stack.length - 1]);
+      if (closeTags[currIndex] === letter) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
 /**
  * Функция принимает массив имен. Имена могут быть в любом виде: пЕтя, Артём, катя, МаШа
  * Функция должна вернуть строку с нормализованными именами, разделёнными запятой и пробелом: "Петя, Артём, Катя, Маша"
  */
-function getCapitalizedNamesString(names) {}
+function getCapitalizedNamesString(names) {
+  return names
+    .map(name => name[0].toUpperCase() + name.toLowerCase().substring(1))
+    .join(", ");
+}
 
 /**
  * Функция принимает массив объектов вида { id: 1, name: "Иван", age: 42 }
  * Функция должна вернуть массив таких же объектов, отсортированных по именам (в алфавитном порядке). НЕ ИСПОЛЬЗУЙТЕ ЦИКЛЫ.
  */
-function sortByName(users) {}
+function sortByName(users) {
+  return users.sort(({ name: aName }, { name: bName }) =>
+    aName.localeCompare(bName)
+  );
+}
 
 /**
  * Функция принимает массив объектов вида { id: 1, name: "Иван", age: 42 }
  * Функция должна вернуть массив имён пользователей которым исполнилось 18 лет. НЕ ИСПОЛЬЗУЙТЕ ЦИКЛЫ.
 
  */
-function getEligibleUserNames(users) {}
+function getEligibleUserNames(users) {
+  return users.filter(({ age }) => age >= 18).map(({ name }) => name);
+}
 
 /**
  * Функция принимает массив покупок вида { name: "Морковь", amount: 3, price: 2.50 }
  * Функция должна вернуть сумму всех покупок. НЕ ИСПОЛЬЗУЙТЕ ЦИКЛЫ.
  */
-function calculateCart(items) {}
+function calculateCart(items) {
+  return items.reduce(
+    (prevVal, { amount, price }) => (prevVal += amount * price),
+    0
+  );
+}
 
 /**
  * Функция принимает массив пользователей вида { id: 1, name: "Иван", access: 3 }
@@ -44,7 +78,9 @@ function calculateCart(items) {}
  * Функция должна вернуть true если у всех пользователей есть доступ на запись или false в противном случае
  * 11 10
  */
-function isAllPermittedToWrite(users) {}
+function isAllPermittedToWrite(users) {
+  return users.every(({ access }) => access === 3 || access === 2);
+}
 
 module.exports = {
   trueLength,
