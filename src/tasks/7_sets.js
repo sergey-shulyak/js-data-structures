9729fbacb76cd97e42ef7a629031ef9605239d86
Vlad Malyarenko <vladislav.malyarenko@nixsolutions.com>
2018-11-23 18:32:16 +0200

    Task 7. Fix. Complete

--- src/tasks/7_sets.js
+++ src/tasks/7_sets.js
@@ -2,14 +2,24 @@
  * Функция принимает массив чисел
  * Функция должна вернуть массив чисел без дубликатов
  */
-function uniqueValues(numbers) {}
+function uniqueValues(numbers) {
+  let set = new Set(numbers);
+  return Array.from(set);
+}
 
 /**
  * Функция принимает 2 множества (Set)
  * Функция должна вернуть множество элементов, которые не находятся в обоих множествах одновременно
  * [1, 2, 3, 4, 5] и [1, 3, 7, 10] => [2, 4, 5, 7, 10]
  */
-function getNotIntersectingElements(setA, setB) {}
+function getNotIntersectingElements(setA, setB) {
+  let set = setA;
+
+  for (let el of setB) {
+    set.has(el) ? set.delete(el) : set.add(el);
+  }
+  return set;
+}
 
 module.exports = {
   uniqueValues,
