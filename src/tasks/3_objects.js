42b673d73c707ec4c46552b1fbf9671128a395f2
Eugene Bondar <bondar.e@nixsolutions.com>
2018-11-23 15:11:40 +0200

    finished module objects

--- src/tasks/3_objects.js
+++ src/tasks/3_objects.js
@@ -2,31 +2,51 @@
  * Функция принимает объект, имя свойства которое нужно изменить и новое имя свойства
  * Функция должна "переименовать" свойство в полученном объекте
  */
-function renameProperty(obj, propName, newName) {}
+function renameProperty(obj, propName, newName) {
+  obj[newName] = obj[propName];
+  delete obj[propName];
+  return obj;
+}
 
 /**
  * Функция принимает объект
  * Функция должна вернуть количество свойств объекта (числом)
  */
-function countKeys(obj) {}
+function countKeys(obj) {
+  return Object.keys(obj).length;
+}
 
 /**
  * Функция принимает объект
  * Функция должна вернуть true если в объекте нет свойств или false в противном случае
  */
-function isEmpty(obj) {}
+function isEmpty(obj) {
+  return Object.keys(obj).length > 0 ? false : true;
+}
 
 /**
  * Функция принимает объект
  * Функция должна вернуть сумму значений свойств, где значение - число
  */
-function numericPropsSum(obj) {}
+function numericPropsSum(obj) {
+  let result = 0;
+
+  Object.values(obj).forEach(val => {
+    if (typeof val === "number") {
+      result += val;
+    }
+  });
+
+  return result;
+}
 
 /**
  * Функция принимает объект
  * Функция должна вернуть независимую копию объекта
  */
-function clone(obj) {}
+function clone(obj) {
+  return { ...obj };
+}
 
 module.exports = {
   renameProperty,
