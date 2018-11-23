7cf0209dbb7250d9295f403acb5512604111f474
Vlad Malyarenko <vladislav.malyarenko@nixsolutions.com>
2018-11-23 18:32:04 +0200

    Task 6. Fix. Complete

--- src/tasks/6_maps.js
+++ src/tasks/6_maps.js
@@ -3,13 +3,26 @@
  * Функция должна вернуть Map, содержащую количество каждой буквы: { a => 3, m => 6, ... }
  * Порядок пар не важен. Буквы А и а считать одной буквой.
  */
-function countLetters(text) {}
+function countLetters(text) {
+  let str = text.toLowerCase();
+  let res = new Map();
+  for (let letter of str) {
+    if (letter === " ") continue;
+    res.set(letter, res.has(letter) ? res.get(letter) + 1 : 1);
+  }
+  return res;
+}
 
 /**
  * Функция принимает Map с парами вида { id: 1, name: "Петя" } => [8, 9, 8, ...]
  * Функция должна вернуть массив строк вида "1 Петя: 25 балл(ов)" где 25 - сумма оценок из массива
  */
-function prettyPrintEntries(userMarksMap) {}
+function prettyPrintEntries(userMarksMap) {
+  let accumulator = (prevVal, currVal) => prevVal + currVal;
+  return Array.from(userMarksMap).map(([user, marks]) => {
+    return `${user.id} ${user.name}: ${marks.reduce(accumulator, 0)} балл(ов)`;
+  });
+}
 
 module.exports = {
   countLetters,
