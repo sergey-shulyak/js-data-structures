4dd7fb0301750b1450bb64f92d52b7588ef2ec6b
Ivan Gaenko <gaenko37@gmail.com>
2018-11-23 17:19:28 +0200

    4_arrays.js

--- src/tasks/4_arrays.js
+++ src/tasks/4_arrays.js
@@ -2,19 +2,62 @@
  * Функция принимает массив
  * Функция должна вернуть "настоящюю длинну массива" [1,2,3,4,5] => 5 | [1,2,3,4, ... пустота... 10] => 5
  */
-function trueLength(array) {}
+function trueLength(array) {
+  var count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (isNaN(array[i]) === false) {
+      count++;
+    };
+  }
+  return count;
+}
 
 /**
  * Функция принимает строку, содержащую последовательность круглых, квадратных или фигурных скобок - "let obj = { numArray: [ ((1 + 2) * 5) + (42).toString() ] }"
  * Функция должна вернуть true если все скобки закрыты в правильном порядке или false в противном случае
  */
-function isExpressionValid(expression) {}
+function isExpressionValid(expression) {
+  var cer = 0;
+  var sqr = 0;
+  var fig = 0;
+  let arr = expression.split("");
+  for (let i = 0; i < arr.length; i++) {
+      if ( arr[i] === "(" || arr[i] === ")") {
+        cer++;
+      }
+      else if ( arr[i] === "[" || arr[i] === "]") {
+        sqr++;
+      }
+      else if ( arr[i] === "{" || arr[i] === "}") {
+        fig++;
+      };
+    };
+    // return fig;
+    if ( cer%2 == 0 && sqr%2 == 0 && fig%2 == 0) {
+      return true;
+    } else {
+      return false;
+    };
+  }
 
 /**
  * Функция принимает массив имен. Имена могут быть в любом виде: пЕтя, Артём, катя, МаШа
  * Функция должна вернуть строку с нормализованными именами, разделёнными запятой и пробелом: "Петя, Артём, Катя, Маша"
  */
-function getCapitalizedNamesString(names) {}
+function getCapitalizedNamesString(names) {
+  var mas = [];
+  var nam;
+  for ( let i = 0; i < names.length; i++ ) {
+    nam = names[i].toLowerCase().split("");
+    naM = nam[0].toUpperCase();
+    nam.shift();
+    nam2 = nam.join("");
+    full = naM + nam2;
+    mas.push(full);
+  }
+  masone = mas.join(", ");
+  return masone;
+}
 
 /**
  * Функция принимает массив объектов вида { id: 1, name: "Иван", age: 42 }
@@ -27,7 +70,11 @@ function sortByName(users) {}
  * Функция должна вернуть массив имён пользователей которым исполнилось 18 лет. НЕ ИСПОЛЬЗУЙТЕ ЦИКЛЫ.
 
  */
-function getEligibleUserNames(users) {}
+function getEligibleUserNames(users) {
+  // for ( let i = 0; i < users.length; i++) {
+  return users.price;
+  // }
+}
 
 /**
  * Функция принимает массив покупок вида { name: "Морковь", amount: 3, price: 2.50 }
