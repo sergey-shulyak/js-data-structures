5966ee3d33d97683a740261e46fe374cba67c7bb
Vadim Bilyuk <vabilyuk@gmail.com>
2018-11-23 13:52:33 +0200

    objects test complited

--- src/tasks/3_objects.js
+++ src/tasks/3_objects.js
@@ -2,31 +2,57 @@
  * Функция принимает объект, имя свойства которое нужно изменить и новое имя свойства
  * Функция должна "переименовать" свойство в полученном объекте
  */
-function renameProperty(obj, propName, newName) {}
+function renameProperty(obj, propName, newName) {
+  obj[newName] = "";
+  for (let key in obj) {
+    if (key === propName) {
+      obj[newName] = obj[key];
+      delete obj.key;
+      return obj;
+    }
+  }
+}
 
 /**
  * Функция принимает объект
  * Функция должна вернуть количество свойств объекта (числом)
  */
-function countKeys(obj) {}
+function countKeys(obj) {
+  return Object.keys(obj).length;
+}
 
 /**
  * Функция принимает объект
  * Функция должна вернуть true если в объекте нет свойств или false в противном случае
  */
-function isEmpty(obj) {}
+function isEmpty(obj) {
+  if (Object.keys(obj).length != 0) {
+    return false;
+  }
+  return true;
+}
 
 /**
  * Функция принимает объект
  * Функция должна вернуть сумму значений свойств, где значение - число
  */
-function numericPropsSum(obj) {}
+function numericPropsSum(obj) {
+  let value = 0;
+  for (let key in obj) {
+    if (typeof obj[key] === "number") {
+      value += obj[key];
+    }
+  }
+  return value;
+}
 
 /**
  * Функция принимает объект
  * Функция должна вернуть независимую копию объекта
  */
-function clone(obj) {}
+function clone(obj) {
+  return Object.assign({}, obj);
+}
 
 module.exports = {
   renameProperty,
