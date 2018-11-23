639c6f7d6dfa3af5d11b18e0852f22fbde32abc9
Victoria Sokolova <viktoriia.sokolova@nure.ua>
2018-11-23 18:23:41 +0200

    Sets tasks done

--- src/tasks/7_sets.js
+++ src/tasks/7_sets.js
@@ -2,14 +2,21 @@
  * Функция принимает массив чисел
  * Функция должна вернуть массив чисел без дубликатов
  */
-function uniqueValues(numbers) {}
+function uniqueValues(numbers) {
+  return [...new Set(numbers)];
+}
 
 /**
  * Функция принимает 2 множества (Set)
  * Функция должна вернуть множество элементов, которые не находятся в обоих множествах одновременно
  * [1, 2, 3, 4, 5] и [1, 3, 7, 10] => [2, 4, 5, 7, 10]
  */
-function getNotIntersectingElements(setA, setB) {}
+function getNotIntersectingElements(setA, setB) {
+  return new Set([
+    ...new Set([...setA].filter(x => !setB.has(x))),
+    ...new Set([...setB].filter(x => !setA.has(x)))
+  ]);
+}
 
 module.exports = {
   uniqueValues,
