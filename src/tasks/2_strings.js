dd4bcffe679e6e8fb12ec2c890f90e9724624640
Sergey Mogilevskiy <sergey.mogilevskiy@nixsolutions.com>
2018-11-23 12:14:06 +0200

    Did 'numbers' tasls

--- src/tasks/2_strings.js
+++ src/tasks/2_strings.js
@@ -2,14 +2,19 @@
  * Функция принимает id, имя и диаметр планеты
  * Функция должна вернуть строку в формате "<id><tab>|<name>|<tab><diameter>"
  */
-function constructPlanetInfoString(id, name, diameter) {}
+function constructPlanetInfoString(id, name, diameter) {
+  return id + "\t|" + name + "|\t" + diameter;
+}
 
 /**
  * Функция принимает какой-то текст
  * Функция должна вернуть true, если текст содержит слова из списка spamWords или false если не содержит.
  * Функция не должна быть чувствительна к регистру (если слово в списке спама "новинка", а в тексте "НоВиНкА" - то это спам)
  */
-function isSpam(text, spamWords) {}
+function isSpam(text, spamWords) {
+  text = text.toLowerCase();
+  return text.indexOf(spamWords) != -1;
+}
 
 /**
  * Функция принимает текст, и значение которое нужно подставить между {{ и }}
