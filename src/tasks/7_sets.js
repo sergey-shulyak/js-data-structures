09c73419f5e6960e9e11eb1a341dabf8370c43bc
Vadim Bilyuk <vabilyuk@gmail.com>
2018-11-26 10:55:02 +0200

    sets test complited

--- src/tasks/7_sets.js
+++ src/tasks/7_sets.js
@@ -2,14 +2,33 @@
  * Функция принимает массив чисел
  * Функция должна вернуть массив чисел без дубликатов
  */
-function uniqueValues(numbers) {}
+function uniqueValues(numbers) {
+  let set = new Set();
+  let tempArr = [];
+  numbers.forEach(element => {
+    set.add(element);
+  });
+  set.forEach(element => {
+    tempArr.push(element);
+  });
+  return tempArr;
+}
 
 /**
  * Функция принимает 2 множества (Set)
  * Функция должна вернуть множество элементов, которые не находятся в обоих множествах одновременно
  * [1, 2, 3, 4, 5] и [1, 3, 7, 10] => [2, 4, 5, 7, 10]
  */
-function getNotIntersectingElements(setA, setB) {}
+function getNotIntersectingElements(setA, setB) {
+  let set = new Set();
+  setA.forEach(element => {
+    if (!setB.has(element)) set.add(element);
+  });
+  setB.forEach(element => {
+    if (!setA.has(element)) set.add(element);
+  });
+  return set;
+}
 
 module.exports = {
   uniqueValues,
